a7786f127d150b497b61796b43f13e2448ab6770
Knud Åge Hansen <kaah@statsbiblioteket.dk>
2020-01-13 14:00:26 +0100

    The service gives either a Perceptive Hash (PHash) or a Difference Hash (DHash) of an image (input is the filepath) URL to get the hash value looks like http://localhost:8080/ds-pictureHash/api/imageDHash/imgPath/?imgPath=<filepath>

--- ds-pictureHash-ws/src/main/java/dk/kb/api/webservice/ApiServiceImpl.java
+++ ds-pictureHash-ws/src/main/java/dk/kb/api/webservice/ApiServiceImpl.java
@@ -1,32 +1,72 @@
 package dk.kb.api.webservice;
 
+import com.github.kilianB.hash.Hash;
+import com.github.kilianB.hashAlgorithms.DifferenceHash;
+import com.github.kilianB.hashAlgorithms.HashingAlgorithm;
+import com.github.kilianB.hashAlgorithms.PerceptiveHash;
 import dk.kb.api.DefaultApi;
-import dk.kb.model.HelloReplyDto;
-import dk.kb.model.IdMetaPairsDto;
+import dk.kb.model.HashReplyDto;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
-import java.util.List;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class ApiServiceImpl  implements DefaultApi {
+    /**
+     * Returns the discrete hash value of the image in imgPath
+     * @param imgPath The path to the image given to the webservice.
+     * @return The discrete hash value
+     */
     @Override
-    public HelloReplyDto getGreeting(String alternateHello) {
-        HelloReplyDto helloReplyDto = new HelloReplyDto();
-        helloReplyDto.setMessage("Hello back");
-        return helloReplyDto;
+    public HashReplyDto getImageDHash(String imgPath) {
+        HashReplyDto HashReplyDto = new HashReplyDto();
+        try {
+            File imgHash = getFile(imgPath);
+            HashingAlgorithm dHashAlg = new DifferenceHash(64, DifferenceHash.Precision.Triple);
+            doHash(HashReplyDto, imgHash, dHashAlg);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return HashReplyDto;
     }
 
+    /**
+     * Returns the perceptive hash value of the image in imgPath
+     * @param imgPath The path to the image given to the webservice.
+     * @return The perceptive hash value
+     */
     @Override
-    public String getImagePHash(String imgPath) {
-        return null;
+    public HashReplyDto getImagePHash(String imgPath) {
+        HashReplyDto HashReplyDto = new HashReplyDto();
+        try {
+            File imgHash = getFile(imgPath);
+            HashingAlgorithm pHashAlg = new PerceptiveHash(64);
+            doHash(HashReplyDto, imgHash, pHashAlg);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return HashReplyDto;
     }
 
-    @Override
-    public  List<IdMetaPairsDto> getMetaData(List<String> ids, String format) {
-        return null;
+    private void doHash(HashReplyDto hashReplyDto, File imgHash, HashingAlgorithm hashingAlgorithm) throws IOException {
+        Hash hash = null;
+        hash = hashingAlgorithm.hash(imgHash);
+        hashReplyDto.setMessage(hash.getHashValue().toString());
     }
 
-    @Override
-    public File getResource(String id) {
-        return null;
+    private static final String filePath= "~/Pictures/imgPHash";
+    private File getFile(String imgPath) throws IOException {
+        URL webImage;
+        webImage = new URL(imgPath);
+        File imgHash = new File(ApiServiceImpl.filePath);
+        FileUtils.copyURLToFile(webImage, imgHash);
+        return imgHash;
     }
 }
