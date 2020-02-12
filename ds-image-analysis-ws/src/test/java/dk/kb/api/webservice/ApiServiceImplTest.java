d6e3ea84050d905ad7b83a1b061e5e5b6f7c3367
Knud Åge Hansen <kaah@statsbiblioteket.dk>
2020-02-12 13:30:46 +0100

    The precision of the hash values are entered in the web interface as start/stop values The hash value is returned in JSON format

--- ds-image-analysis-ws/src/test/java/dk/kb/api/webservice/ApiServiceImplTest.java
+++ ds-image-analysis-ws/src/test/java/dk/kb/api/webservice/ApiServiceImplTest.java
@@ -11,16 +11,16 @@ class ApiServiceImplTest {
     void getImageDHash() {
         String url = "http://kb-images.kb.dk/DAMJP2/online_master_arkiv/non-archival/KOB/bs_kistebilleder-2/bs000001/full/!345,2555/0/native.jpg";
         ApiServiceImpl tester = new ApiServiceImpl();
-        String result = tester.getImageDHash(url).toString();
+        String result = tester.getImageDHash(url, 1, 8).toString();
         Assertions.assertTrue(result.contains("message: 379723421694405058899193996058950688972138258217989017426890367"));
         url = "https://kb-images.kb.dk/DAMJP2/online_master_arkiv/non-archival/KOB/bs_kistebilleder-2/bs000001/full/!345,2555/0/native.jpg";
-        result = tester.getImageDHash(url).toString();
+        result = tester.getImageDHash(url, 1, 8).toString();
         Assertions.assertTrue(result.contains("message: null"));
-        result = tester.getImageDHash(null).toString();
+        result = tester.getImageDHash(null, 1, 8).toString();
         Assertions.assertTrue(result.contains("message: null"));
-        result = tester.getImageDHash("").toString();
+        result = tester.getImageDHash("", 1, 8).toString();
         Assertions.assertTrue(result.contains("message: null"));
-        result = tester.getImageDHash("https://test").toString();
+        result = tester.getImageDHash("https://test",  1, 8).toString();
         Assertions.assertTrue(result.contains("message: null"));
     }
 
@@ -28,16 +28,16 @@ class ApiServiceImplTest {
     void getImagePHash() {
         String url = "http://kb-images.kb.dk/DAMJP2/online_master_arkiv/non-archival/KOB/bs_kistebilleder-2/bs000001/full/!345,2555/0/native.jpg";
         ApiServiceImpl tester = new ApiServiceImpl();
-        String result = tester.getImagePHash(url).toString();
+        String result = tester.getImagePHash(url,  1, 8).toString();
         Assertions.assertTrue(result.contains("message: 17667239340915791203"));
         url = "https://kb-images.kb.dk/DAMJP2/online_master_arkiv/non-archival/KOB/bs_kistebilleder-2/bs000001/full/!345,2555/0/native.jpg";
-        result = tester.getImagePHash(url).toString();
+        result = tester.getImagePHash(url,  1, 8).toString();
         Assertions.assertTrue(result.contains("message: null"));
-        result = tester.getImagePHash(null).toString();
+        result = tester.getImagePHash(null,  1, 8).toString();
         Assertions.assertTrue(result.contains("message: null"));
-        result = tester.getImagePHash("").toString();
+        result = tester.getImagePHash("",  1, 8).toString();
         Assertions.assertTrue(result.contains("message: null"));
-        result = tester.getImagePHash("https://test").toString();
+        result = tester.getImagePHash("https://test",  1, 8).toString();
         Assertions.assertTrue(result.contains("message: null"));
     }
 
@@ -45,29 +45,23 @@ class ApiServiceImplTest {
     void getHashDistance() {
         ApiServiceImpl tester = new ApiServiceImpl();
         String result = "";
-        result = tester.getHashDistance("4;1").toString();
+        result = tester.getHashDistance("4","1").toString();
         Assertions.assertTrue(result.contains("message: 2"));
-        result = tester.getHashDistance("-4;1").toString();
+        result = tester.getHashDistance("-4","1").toString();
         Assertions.assertTrue(result.contains("message: Both hash values has to be positive"));
-        result = tester.getHashDistance("4;-11").toString();
+        result = tester.getHashDistance("4","-11").toString();
         Assertions.assertTrue(result.contains("message: Both hash values has to be positive"));
-        result = tester.getHashDistance("-4;-11").toString();
+        result = tester.getHashDistance("-4","-11").toString();
         Assertions.assertTrue(result.contains("message: Both hash values has to be positive"));
-        result = tester.getHashDistance("abc;def").toString();
-        Assertions.assertTrue(result.contains("message: Both values has to be BigInteger separated by ; character"));
-        result = tester.getHashDistance("abc;def;ghi").toString();
-        Assertions.assertTrue(result.contains("message: Both values has to be BigInteger separated by ; character"));
-        result = tester.getHashDistance("").toString();
-        Assertions.assertTrue(result.contains("message: The two hash values has to be divided by ; character"));
-        result = tester.getHashDistance(null).toString();
-        Assertions.assertTrue(result.contains("message: The two hash values has to be divided by ; character"));
-        result = tester.getHashDistance("4;;1").toString();
-        Assertions.assertTrue(result.contains("message: Both values has to be BigInteger separated by ; character"));
-        result = tester.getHashDistance("4;4").toString();
+        result = tester.getHashDistance("abc","def").toString();
+        Assertions.assertTrue(result.contains("message: Both values has to be BigInteger"));
+        result = tester.getHashDistance("", "").toString();
+        Assertions.assertTrue(result.contains("message: The two hash values has to be present"));
+        result = tester.getHashDistance(null, null).toString();
+        Assertions.assertTrue(result.contains("message: The two hash values has to be present"));
+        result = tester.getHashDistance("4","4").toString();
         Assertions.assertTrue(result.contains("message: 0"));
-        result = tester.getHashDistance("4;;1").toString();
-        Assertions.assertTrue(result.contains("message: Both values has to be BigInteger separated by ; character"));
-        result = tester.getHashDistance("00100011;100011100").toString();
+        result = tester.getHashDistance("00100011","100011100").toString();
         Assertions.assertTrue(result.contains("message: 19"));
     }
 }
--- kb-s2i/build.sh
+++ kb-s2i/build.sh
@@ -1,7 +1,7 @@
 
 cd /tmp/src
 
-cp -rp /tmp/src/target/ds-pictureHash-*.war $TOMCAT_APPS/ds-pictureHash.war
-cp /tmp/src/conf/ocp/ds-pictureHash.xml $TOMCAT_APPS/ds-pictureHash.xml
+cp -rp /tmp/src/target/ds-image-analysis-*.war $TOMCAT_APPS/ds-image-analysis.war
+cp /tmp/src/conf/ocp/ds-image-analysis.xml $TOMCAT_APPS/ds-image-analysis.xml
 
-export WAR_FILE=$(readlink -f $TOMCAT_APPS/ds-pictureHash.war)
+export WAR_FILE=$(readlink -f $TOMCAT_APPS/ds-image-analysis.war)
